d8df1e9599a770fc024223ad599993df12202ae9
wojjas <wojtek@proxoft.se>
2014-10-28 12:37:32 +0300

    Has MessageController

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Journal.Controllers
+{
+    public class MessageController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Message GET: Test message")
+            };
+        }
+        //public HttpResponseMessage Post()
+        //{
+        //    return new HttpResponseMessage()
+        //    {
+        //        Content = new StringContent("Message POST: Test message")
+        //    };
+        //}
+        public JsonResult<JournalMessageOutputModel> Post(JournalMessageInputModel messageModel)
+        {
+            JsonResult<JournalMessageOutputModel> retValue = null;
+
+            //TODO: Implement persistance logic here, eg. save to DB
+            if (messageModel != null && !string.IsNullOrEmpty(messageModel.Message))
+            {
+                string fragment = messageModel.Message.PadRight(6, ' ').Substring(0, 6) + "...";
+                var tmp = new JournalMessageOutputModel() { Status = "Posted journal entry to server: '" + fragment + "'" };
+                retValue = Json(tmp);
+            }
+            else
+            {
+                var tmp = new JournalMessageOutputModel() { Status = "Empty message request." };
+                retValue = Json(tmp);
+            }
+
+            return retValue;
+        }
+    }
+}
